3160e2d77d4dfe6b7211123d52d92f6306519aaa
Sean Okai <seanokai@hotmail.com>
2015-04-23 07:21:09 -0400

    aded var js scripts for the different properties.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 var $btnShowHide = $('.btn-show-hide');
-
+var $btnShowHide = $('.btn-show-hide');
+var $box = $('.box');
+var $btnMove = $('.btn-move');
+var $diamond = $('.diamond');
+var $btnCollapseExpand = $('.btn-collapse-expand');
+var $panel = $('.panel');
+var $btnBounce = $('.btn-bounce');
+var $circle = $('.circle');
+var $btnAppend = $('.btn-append');
+var $list = $('.list');
 
 $btnShowHide.on('click', function (){
     $box.toggleClass('js-btn-show');
