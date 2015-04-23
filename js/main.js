d9a0c3c82d9d94d7a8fd3dc173fb74b0131e3041
Sean Okai <seanokai@hotmail.com>
2015-04-23 05:36:11 -0400

    added javascript for show/hide green box

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var $btnShowHide = $('.btn-show-hide');
+
+var $box = $('.box');
+
+
+$btnShowHide.on('click', function (){
+    $box.toggleClass('js-btn-show');
+//console.log ('btnShowHide');
+});
+
+
